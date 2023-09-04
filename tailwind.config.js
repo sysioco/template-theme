9d0f062f5796f8515f6a451dc5bbd6cf4b5575d5
Derek <derek@sysio.co>
2023-09-04 12:08:14 +0100

    implemented daisyui and a custom theme for starter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './templates/**/*.{liquid,json}',
@@ -9,5 +8,22 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#f0acf9',
+          secondary: '#eabe4f',
+          accent: '#f74c90',
+          neutral: '#231b27',
+          'base-100': '#474649',
+          info: '#369ee7',
+          success: '#5fddb7',
+          warning: '#fcd340',
+          error: '#f11e59',
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
 };
