e6ed6c047413c76065ed7558561055179de091e7
Derek <derek@sysio.co>
2023-11-13 19:44:48 +0000

    feature finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,23 +8,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: '#f0acf9',
-          secondary: '#eabe4f',
-          accent: '#f74c90',
-          neutral: '#231b27',
-          'base-100': '#474649',
-          info: '#369ee7',
-          success: '#5fddb7',
-          warning: '#fcd340',
-          error: '#f11e59',
-        },
-      },
-      'cupcake',
-    ],
-  },
-  plugins: [require('daisyui')],
+  plugins: [],
 };
