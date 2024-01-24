ad53cac991b70d78b32ad3eb3d8f3033dfc0c465
Derek <derek@sysio.co>
2024-01-24 15:27:50 +0000

    added styling to header, removed alpinejs, will attach either react or vue instead

--- src/app.js
+++ src/app.js
@@ -1,9 +1,1 @@
-import Alpine from 'alpinejs';
-import AnnouncementBar from './AnnouncementBar';
-
-window.Alpine = Alpine;
-
-// Components
-Alpine.data('AnnouncementBar', AnnouncementBar);
-
-Alpine.start();
+console.log('webpack');
